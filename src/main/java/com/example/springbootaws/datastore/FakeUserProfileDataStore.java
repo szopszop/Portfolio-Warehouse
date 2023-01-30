d4d719a0e70e37d46b6226b946883bd4c4e1b480
szopszop <szymontracz1@gmail.com>
2023-01-30 14:34:41 +0100

    first api with user profiles

--- src/main/java/com/example/springbootaws/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/example/springbootaws/datastore/FakeUserProfileDataStore.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public class UserProfileDataStore {
+public class FakeUserProfileDataStore {
 
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
