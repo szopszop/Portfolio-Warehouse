73948b2cc48efe24567232aa460a5dc44c8e99d8
szopszop <szymontracz1@gmail.com>
2023-02-23 16:33:41 +0100

    check

---
+++ src/main/java/com/example/springbootaws/security/OAuthConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.springbootaws.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
+
+
+}
