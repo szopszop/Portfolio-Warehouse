f6da923b2face6a1f7924008186e59034711bfcd
szopszop <szymontracz1@gmail.com>
2023-02-23 20:01:08 +0100

    updated pom, user DTO

--- src/main/java/com/szymontracz/portfoliowarehouse/controller/UserController.java
+++ src/main/java/com/szymontracz/portfoliowarehouse/controller/UserController.java
@@ -2,7 +2,8 @@ package com.szymontracz.portfoliowarehouse.controller;
 
 import com.szymontracz.portfoliowarehouse.security.MessageResponse;
 import com.szymontracz.portfoliowarehouse.service.UserService;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +11,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
-@RequiredArgsConstructor
+@Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping("api/v1/user")
 @CrossOrigin("http://localhost:5173/")
@@ -19,11 +21,8 @@ public class UserController {
     private final UserService userService;
 
 
-    @PostMapping(
-            path = "{userProfileId}/image/upload",
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @PostMapping(path = "{userProfileId}/image/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+                                                        produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MessageResponse> uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
                                                                   @RequestParam("imageFile")MultipartFile imageFile) {
         return userService.uploadProfilePictureValidation(userProfileId, imageFile);
--- src/main/java/com/szymontracz/portfoliowarehouse/model/UserDto.java
+++ src/main/java/com/szymontracz/portfoliowarehouse/model/UserDto.java
@@ -1,12 +1,23 @@
 package com.szymontracz.portfoliowarehouse.model;
 
 import com.szymontracz.portfoliowarehouse.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
 import java.util.UUID;
 
+@Builder
+@Data
+@ToString
 public class UserDto {
 
     private UUID id;
+
+    @NotBlank
     private String username;
+    @NotBlank
     private String email;
     private String userProfileImageLink;
     private String password;
