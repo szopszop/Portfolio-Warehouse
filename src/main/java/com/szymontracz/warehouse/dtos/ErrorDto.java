5beae732a7a2d1b3207e299097eb8ee5e8495018
szopszop <szymontracz1@gmail.com>
2023-03-04 11:51:30 +0100

    added auth entry point with dto

---
+++ src/main/java/com/szymontracz/warehouse/dtos/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.szymontracz.warehouse.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class ErrorDto {
+
+    private String message;
+}
