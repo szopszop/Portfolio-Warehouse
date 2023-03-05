13ab615bddaabf90e5a1f6b7c76a84552cb941e5
szopszop <szymontracz1@gmail.com>
2023-03-05 12:12:05 +0100

    fresh start vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,34 @@
-import React, {useState, useEffect} from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import './App.css'
-import axios from "axios";
-import Dropzone from "./components/Dropzone/Dropzone.jsx";
-
-const UserProfiles = () => {
-
-    const [userProfiles, setUserProfiles] = useState([]);
-
-    const fetchUserProfiles = () => {
-        axios.get("http://localhost:8080/api/v1/user-profile").then( res => {
-            console.log(res);
-            setUserProfiles(res.data)
-        });
-    }
-
-    useEffect( () => {
-        fetchUserProfiles();
-    }, [])
-
-    return userProfiles.map( (userProfile) => {
-
-        return (<div key={userProfile.userProfileId}>
-            {userProfile.userProfileImageLink ?
-                <img className="profile-picture" src={`http://localhost:8080/api/v1/user-profile/${userProfile.userProfileId}/image/download`}
-                     alt="profile-picture"/> : null}
-            <br />
-            <br />
-
-            <h1>{userProfile.username}</h1>
-            <p>{userProfile.userProfileId}</p>
-            <Dropzone userProfileId={userProfile.userProfileId}/>
-            <br />
-        </div>)
-    })
-}
-
-
-
 
 function App() {
+  const [count, setCount] = useState(0)
 
-    return (
-        <div className="App">
-            <div>
-                <UserProfiles />
-            </div>
-        </div>
-    )
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
 
 export default App
