b249de7adb74b9795fbe9b54cdba46e53c764cfd
szopszop <szymontracz1@gmail.com>
2023-03-10 15:58:23 +0100

    updated env

--- backend/src/main/java/com/szymontracz/warehouse/PortfolioWarehouseApp.java
+++ backend/src/main/java/com/szymontracz/warehouse/PortfolioWarehouseApp.java
@@ -2,12 +2,13 @@ package com.szymontracz.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PortfolioWarehouseApp {
     
     public static void main(String[] args) {
