308db3cf372ae1d8e844c23800150fda6bf20a35
szopszop <szymontracz1@gmail.com>
2023-03-11 09:28:45 +0100

    updated render.yaml

--- backend/src/main/java/com/szymontracz/warehouse/PortfolioWarehouseApp.java
+++ backend/src/main/java/com/szymontracz/warehouse/PortfolioWarehouseApp.java
@@ -2,8 +2,7 @@ package com.szymontracz.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class PortfolioWarehouseApp {
     
     public static void main(String[] args) {
